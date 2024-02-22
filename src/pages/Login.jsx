cf3bb9f739dcb3c9727d70b029f3ef9e1bcf150e
samyam <samyamshr@gmail.com>
2024-02-22 15:02:45 +0545

    log in page and regiter page done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled from 'styled-components'
+import { mobile } from '../responsive';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp8448704.png&f=1&nofb=1&ipt=c859b31637bc1af6802e0f938023e923d60a9a1182ec9b1c1d4b3de0e7aae234&ipo=images")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: #D3F5F5;
+  ${mobile({ width: "75%" })}
+  border-radius:20px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+  border:none;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Button>LOGIN</Button>
+          <Link>Forgotten password?</Link>
+          <Link>Create a new account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
