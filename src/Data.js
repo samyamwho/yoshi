880ca4888cbc2b4d3ed041e2f8d6d542da424fee
samyam <samyamshr@gmail.com>
2024-02-27 00:06:52 +0545

    project completion

--- src/Data.js
+++ src/Data.js
@@ -1,23 +1,23 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://i.ibb.co/XsdmR2c/1.png",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6174cd1a-f614-4c1e-9d75-4997060b6156/ddrsqbl-eb74072b-24d1-4fcf-8981-8526538e676b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYxNzRjZDFhLWY2MTQtNGMxZS05ZDc1LTQ5OTcwNjBiNjE1NlwvZGRyc3FibC1lYjc0MDcyYi0yNGQxLTRmY2YtODk4MS04NTI2NTM4ZTY3NmIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XE0tgzITQOCQo38R05ZFj_RGaVYSEmx5hXh2_l4BJmY",
+      title: "JET COLLECTION",
+      desc: "Prepare to embody the swift and elusive spirit of Jet  with this meticulously crafted cosplay ensemble. Channeling the essence of a nimble duelist, this outfit captures the dynamic and stealthy style that defines this character.",
       bg: "f5fafd",
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://www.valoguides.com/images/agents/viper/agent_viper-min.png",
+      title: "VIPER COLLECTION",
+      desc: "Discover elegance and bold style with our Viper Collection. Immerse yourself in sleek designs inspired by the grace of vipers. Elevate your fashion with these sophisticated pieces that make a statement. Explore the allure of modern luxury in our exclusive Viper Collection",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://i.ibb.co/cXFnLLV/3.png",
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://i.pinimg.com/originals/da/af/dc/daafdc5e760a07c752df645c7e4c5d69.png",
+      title: "OMEN COLLECTION",
+      desc: "Introducing the Omen Collection, where mystique meets modernity. Unleash a curated selection of fashion pieces inspired by enigmatic forces. Elevate your style with these statement-making designs that capture the essence of mystery and allure. Explore the Omen Collection for a fusion of contemporary trends and timeless elegance, creating a wardrobe that exudes sophistication.",
       bg: "fbf0f4",
     },
   ];
@@ -47,10 +47,12 @@ export const sliderItems = [
     {
       id:1,
       img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      category:"White",
     },
     {
       id:2,
       img:"https://i1.wp.com/magic-custom.com/13655-large_product/cactus-jack-t-shirt-noir-cactus-logo-travis-scott.jpg",
+      category:"Black",
     },
     {
       id:3,
@@ -63,17 +65,42 @@ export const sliderItems = [
     {
       id:5,
       img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      category:"White",
     },
     {
       id:6,
       img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      category:"white",
     },
     {
       id:7,
       img:"https://www.pngarts.com/files/1/Jacket-PNG-Image-Background.png",
+      category:"Green",
     },
     {
       id:8,
       img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      category:"White",
     },
+    {
+      id:9,
+      img:"https://i0.wp.com/cuttongarments.com/wp-content/uploads/2017/12/18yellow-gold.png?fit=1000%2C1250&ssl=1",
+      category:"Yellow  ",
+    },
+    {
+      id:10,
+      img:"https://pngimg.com/uploads/jacket/jacket_PNG8058.png",
+      category:"Red",
+    },
+    {
+      id:11,
+      img:"https://merch-billieeilish.com/wp-content/uploads/2021/11/unisex-3d-printed-billie-eilish-hoodie-sweatshirts.png",
+      category:"Green",
+    },
+    {
+      id:11,
+      img:"https://wowjohn.com/wp-content/uploads/2022/05/Blue-Jacket-PNG-High-Quality-Image.png",
+      category:"Blue",
+    },
+
   ]
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 import styled from 'styled-components';
@@ -12,37 +12,41 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  ${mobile({ display: "none" })}
-  `;
+  ${mobile({ display: 'none' })}
+`;
+
 const Wrapper = styled.div`
-  height=100%;
-  display:flex;
+  height: 100%;
+  display: flex;
   transition: all 1.5s ease;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
-  `;
+`;
 
 const Slide = styled.div`
-  width:100vw;
-  height:100vh;
-  display:flex;
-  align-tems:center;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
   background-color: #${(props) => props.bg};
-  `;
+`;
 
 const ImgContainer = styled.div`
   height: 100%;
   flex: 1;
 `;
+
 const Image = styled.img`
   height: 80%;
+  margin-left:150px;
 `;
+
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
 `;
 
 const Title = styled.h1`
-margin-top:120px;
+  margin-top: 10px;
   font-size: 100px;
 `;
 
@@ -63,59 +67,59 @@ const Button = styled.button`
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #fff7f7;
+  background-color: #B2A4D4;
   border-radius: 50%;
-  display:flex;
+  display: flex;
   align-items: center;
-  justify-content:center;
-  position:absolute;
+  justify-content: center;
+  position: absolute;
   top: 0;
   bottom: 0;
   margin: auto;
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
+  left: ${(props) => props.direction === 'left' && '10px'};
+  right: ${(props) => props.direction === 'right' && '10px'};
   cursor: pointer;
-  opacity: 0.5;
+  opacity: 0.2;
   z-index: 2;
 `;
+
 const Slider = () => {
-    const [slideIndex, setSlideIndex] = useState(0);
-    const handleClick = (direction) => {
+  const [slideIndex, setSlideIndex] = useState(0);
 
-    };
-    return (
-        <Container>
-            <Arrow direction="left" onClick={() => handleClick("left")}>
-                <ArrowLeftOutlinedIcon />
-            </Arrow>
-            <Wrapper>
-                {sliderItems.map((item)=>(
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex((prev) => (prev > 0 ? prev - 1 : sliderItems.length - 1));
+    } else if (direction === 'right') {
+      setSlideIndex((prev) => (prev < sliderItems.length - 1 ? prev + 1 : 0));
+    }
+  };
 
-                    <Slide bg={item.bg}>
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick('left')}>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id} bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Link to={`/product/${item.id}`}>
+                <Button>SHOW NOW</Button>
+              </Link>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick('right')}>
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+    </Container>
+  );
+};
 
-                    <ImgContainer>
-                        <Image src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6174cd1a-f614-4c1e-9d75-4997060b6156/ddrsqbl-eb74072b-24d1-4fcf-8981-8526538e676b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYxNzRjZDFhLWY2MTQtNGMxZS05ZDc1LTQ5OTcwNjBiNjE1NlwvZGRyc3FibC1lYjc0MDcyYi0yNGQxLTRmY2YtODk4MS04NTI2NTM4ZTY3NmIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XE0tgzITQOCQo38R05ZFj_RGaVYSEmx5hXh2_l4BJmY" />
-
-                    </ImgContainer>
-                    <InfoContainer>
-                        <Title>Jet Outfit</Title>
-                        <Desc>Prepare to embody the swift and elusive spirit of Jet  with this meticulously crafted cosplay ensemble. Channeling the essence of a nimble duelist, this outfit captures the dynamic and stealthy style that defines this character.</Desc>
-                        <Link to="product/undefined">
-                        <Button>SHOW NOW</Button>
-                        </Link>
-
-                    </InfoContainer>
-                </Slide>
-
-
-                    ))}
-            </Wrapper>
-            <Arrow direction="right" onClick={() => handleClick("right")}>
-                <ArrowRightOutlinedIcon />
-            </Arrow>
-
-        </Container>
-    )
-}
-
-export default Slider
+export default Slider;
